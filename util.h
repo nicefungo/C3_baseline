be6e9d1f1fcfaafb48f4d928b5279b9315252935
zhaoxy <xinyang.zhao1015@gmail.com>
2025-09-09 19:23:42 +0800

    Update Conv1 and a bugged corresponding unit test

---
+++ util.h
@@ -0,0 +1,13 @@
+
+#include <cmath>
+#include <vector>
+
+/*
+template <typename T>
+T gelu_(T x) {
+    const T kAlpha = static_cast<T>(0.7978845608028654); // sqrt(2.0 / PI)
+    const T kBeta = static_cast<T>(0.044715);
+    T x_cubed = x * x * x;
+    return static_cast<T>(0.5f) * x * (1.0f + std::tanh(kAlpha * (x + kBeta * x_cubed)));
+}
+*/
