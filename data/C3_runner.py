f62a2c61e2ce8e4de89d4207efa10c25f1f1a97b
zhaoxy <xinyang.zhao1015@gmail.com>
2025-09-16 11:07:56 +0800

    Add test case generator in /data

---
+++ data/C3_runner.py
@@ -0,0 +1,25 @@
+import onnxruntime as ort
+import torch
+import numpy as np
+import sys
+
+
+session = ort.InferenceSession("C3.onnx")
+n = 10
+if len(sys.argv) > 1:
+    n = int(sys.argv[1])
+
+for i in range(n):
+    data = torch.rand(1, 32, 160, 160).numpy()
+
+    session = ort.InferenceSession("C3.onnx")
+    output = session.run(None, {"input_C3": data})
+
+    np.savetxt(f"inputs/input_{i}.txt", np.reshape(data, (32, 25600)), \
+            fmt='%.7f')
+    np.savetxt(f"outputs/output_{i}.txt", np.reshape(output, (32, 25600)), \
+            fmt='%.7f')
+    #print("[", len(output), ", ", len(output[0]), ", ", len(output[0][0]), \
+    #        ", ", len(output[0][0][0]), ", ", len(output[0][0][0][0]), "]")
+
+    #print(output[0][0][0][0][0])
---
+++ data/makeclean.sh
@@ -0,0 +1,3 @@
+
+rm outputs/*.txt
+rm inputs/*.txt
